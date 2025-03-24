79e5786811d9a71fc19e1dc05e3b1c5d4d4cb668
gahangandi11 <gahangandi11@gmail.com>
2025-03-24 17:15:05 -0500

    app center changes

---
+++ src/interfaces/AppPage.ts
@@ -0,0 +1,12 @@
+export interface AppPage {
+    url: string;
+    iosIcon: string;
+    mdIcon: string;
+    title: string;
+    link?: string;
+    category: string;
+    displayPage: string;
+}
+
+
+
